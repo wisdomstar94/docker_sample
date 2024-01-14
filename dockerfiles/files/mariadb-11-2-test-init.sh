e37b027dc6e6081cf60142a9d62259c7bcb9c5ae
wisdomstar94 <wisdomstar94@gmail.com>
2024-01-14 14:39:04 +0900

    dockerfile 쪽 구조 정리.

--- dockerfiles/files/mariadb-11-2-test-init.sh
+++
@@ -1,8 +0,0 @@
-service mariadb start
-
-if [ -n "$MARIADB_ROOT_PASSWORD" ]
-then
-  (echo "ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('$MARIADB_ROOT_PASSWORD');"; echo "exit";) | mariadb
-else
-  echo "MARIADB_ROOT_PASSWORD 환경변수가 없으므로 수행되지 않음."
-fi
